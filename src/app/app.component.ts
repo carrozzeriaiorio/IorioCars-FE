547519c2cd713e92676532120ad479b615b800d4
Vincenzo Marcantuono <vincenzo.marcantuono@si2001.it>
2025-08-30 16:22:40 +0200

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoaderComponent } from './components/loader/loader.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HttpClientModule],
+  imports: [RouterOutlet, NavbarComponent, LoaderComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
